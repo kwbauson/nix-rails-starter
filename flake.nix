86291419af45f2e655abef895c73e5806de80d25
Keith Bauson <kwbauson@gmail.com>
2022-08-09 12:18:46 -0400

    add rails

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
     devShells.default = with nixpkgs.legacyPackages.${system}; mkShell {
-      buildInputs = let ruby = ruby_3_1; in [ (ruby.withPackages (ps: [ ps.rails ])) ];
+      buildInputs = let ruby = ruby_3_1; in
+        [
+          (ruby.withPackages (ps: [ ps.rails ]))
+          sqlite
+        ];
     };
   });
 }
