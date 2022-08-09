93c6b9e675e02242a86ce5528f78ff6fd2868c02
Keith Bauson <kwbauson@gmail.com>
2022-08-09 12:14:45 -0400

    inital commit

---
+++ flake.nix
@@ -0,0 +1,7 @@
+{
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
+    devShells.default = with nixpkgs.legacyPackages.${system}; mkShell {
+      buildInputs = let ruby = ruby_3_1; in [ (ruby.withPackages (ps: [ ps.rails ])) ];
+    };
+  });
+}
